7e25f3a3ec475cd6753030cab804e6b029ffbc90
unixweb <joachimhummel@JoachimsiMac27.fritz.box>
2015-05-17 18:52:45 +0200

    SQL-Script
    Change order of SQL-rows

--- createTable.sql
+++ createTable.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `temperatures` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `humidity` varchar(20) NOT NULL,
   `dateMeasured` date NOT NULL,
   `hourMeasured` int(128) NOT NULL,
   `temperature_1` double NOT NULL,
@@ -9,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `temperatures` (
   `temperature_4` double NOT NULL,
   `pressure` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `pressure-sea` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `humidity` varchar(20) NOT NULL,
   `altitude` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
